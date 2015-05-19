9d7e9bad67546ae9a3b5f9a0d2cc903894f6f964
Carlos Mateos <carlos.mateos@mynewsdesk.com>
2015-05-19 14:45:00 +0200

    Spotbot has its own module now

--- spotbot-cli.go
+++ spotbot-cli.go
@@ -2,94 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/CloudCom/firego"
+	"github.com/wuiscmc/spotbot-cli/spotbot"
 )
 
-type Track struct {
-	duration float64
-	uri      string
-	title    string
-	image    string
-	artists  interface{}
-}
-
-type Player struct {
-	next    bool
-	playing bool
-}
-
-type Playlist struct {
-	tracks []Track
-}
-type Spotbot struct {
-	rootUrl string
-	fb      *firego.Firebase
-}
-
-func (sp *Spotbot) CurrentTrack() Track {
-	var val map[string]interface{}
-	ref := sp.fb.Child("current_track")
-	logError(ref.Value(&val))
-	return toTrack(val)
-}
-
-func toTrack(val map[string]interface{}) Track {
-	return Track{val["duration"].(float64), val["uri"].(string), val["title"].(string), val["image"].(string), val["artists"].(interface{})}
-}
-
-func (sp *Spotbot) Playing() Playlist {
-	var val []map[string]interface{}
-	ref := sp.fb.Child("playlist")
-	logError(ref.Value(&val))
-	tracks := make([]Track, len(val))
-	for _, trackData := range val {
-		tracks = append(tracks, toTrack(trackData))
-	}
-	playlist := Playlist{tracks}
-	return playlist
-}
-
-func (sp *Spotbot) NextSong() {
-	ref := sp.fb.Child("player/next")
-	logError(ref.Set(true))
-}
-
-func (sp *Spotbot) Shuffle() {
-	ref := sp.fb.Child("playlist/shuffle")
-	shuffle := !sp.IsShuffled()
-	logError(ref.Set(shuffle))
-}
-
-func (sp *Spotbot) IsShuffled() bool {
-	var val bool
-	ref := sp.fb.Child("playlist/shuffle")
-	logError(ref.Value(&val))
-	return val
-}
-
-func (sp *Spotbot) Pause() {
-	ref := sp.fb.Child("player/playing")
-	logError(ref.Set(false))
-}
-
-func (sp *Spotbot) Play() {
-	ref := sp.fb.Child("player")
-	logError(ref.Set(true))
-}
-
-func logError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	fb := firego.New(os.Getenv("FIREBASE_URL"))
-	sp := &Spotbot{fb: fb}
+	firebaseUrl := os.Getenv("FIREBASE_URL")
+	if firebaseUrl == "" {
+		fmt.Println("Please set up your FIREBASE_URL env variable first")
+		return
+	}
+	sp := spotbot.New(firebaseUrl)
 	fmt.Println("%s", sp.CurrentTrack())
 	sp.Playing()
-	//sp.Pause()
 }
